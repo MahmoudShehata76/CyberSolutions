ffefe38ecc7e9ef031ccf9028a25342b447362f2
Mek-awy <mo.k.mekawy@gmail.com>
2026-08-18 14:43:33 +0300

    Update project

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -85,7 +85,7 @@ export default function Solutions() {
 
                               <div className="w-full lg:w-5/12 px-8 text-center">
 
-                                   <img src="/images/HERO/engineering-solutions.jpg"
+                                   <img src="/images/HERO/engineering-solutions.webp"
                                         className="max-w-full h-auto hero-image"
                                         alt="Engineering Solutions" />
 
